c61637e60b774c9c2e6fcf363998931e67cf8310
Anish Kanthamneni <akneni@gmail.com>
2023-08-06 01:03:35 -0400

    Added first assignment.

--- Python/CustomNumpyArray_complete.py
+++ Python/CustomNumpyArray_complete.py
@@ -1,5 +1,4 @@
 class Array():
-    # Constructor. Do not change the code in this function
     def __init__ (self, lst):
         self.data = lst
     
@@ -14,8 +13,6 @@ class Array():
 
     def max(self):
         return max(self.data)
-
-    # This __str__ method should return a string version of the list with curly braces instead of squre brackets
-    # Example: if self.data = [1, 2, 3], the __str__ method should return "{1, 2, 3}"
+    
     def __str__(self):
         return '{' + str(self.data)[1:-1] + '}'
